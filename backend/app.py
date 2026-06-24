f0f3aeebbdc0e099b3055bea118bd389789d4acd
Francky <cobagiika@gmail.com>
2026-06-24 19:16:55 +0200

    fix port

--- backend/app.py
+++ backend/app.py
@@ -158,5 +158,6 @@ def get_reponse():
 
 
 if __name__ == '__main__':
-    print(" Serveur Fanorona-Telo démarré sur http://localhost:5000")
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT' , 5000))
+    app.run(host='0.0.0.0', port=port)
